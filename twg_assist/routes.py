e828161647c49cdf70fc481136914a3f6c3f8626
Zach Goldstein <zgoldstein@google.com>
2016-10-21 18:32:38 -0400

    webhook.

--- twg_assist/routes.py
+++ twg_assist/routes.py
@@ -2,5 +2,5 @@ import webapp2
 from twg_assist.handlers.apiai import APIAIHandler
 
 _APP = webapp2.WSGIApplication([
-    webapp2.Route('/webhooks', handler=APIAIHandler, name='webhook'),
+    webapp2.Route('/webhook', handler=APIAIHandler, name='webhook'),
 ], debug=True)
