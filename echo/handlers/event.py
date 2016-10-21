2c485586f51b37b642a98b40f497288c3533a36d
Ryan Balch <rbalch@hugeinc.com>
2016-10-21 11:35:34 -0400

    finalizing echo admin working

--- echo/handlers/event.py
+++ echo/handlers/event.py
@@ -1,7 +1,10 @@
 from base import BaseEchoHandler
+from echo.models.eventLog import EventLog
 
 
 class EventIndexHandler(BaseEchoHandler):
 
     def get(self):
+        eventLogs = EventLog.query().order(-EventLog.created).fetch(20)
+        self.context['eventLogs'] = eventLogs
         self.render('eventIndex.html')
